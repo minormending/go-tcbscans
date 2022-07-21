c2b203f9a1d1bfd36282fa0d95b1956b2978cea9
Kevin Ramdath <krpent@gmail.com>
2022-07-21 10:18:20 -0400

    use a series obj for series fetch

--- pkg/series.go
+++ pkg/series.go
@@ -7,24 +7,36 @@ import (
 	"regexp"
 )
 
-func GetSeries() (string, error) {
-	resp, err := http.Get("https://onepiecechapters.com/projects")
+type Series struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+}
+
+func GetSeries() ([]Series, error) {
+	page, err := getSeriesPage()
 	if err != nil {
-		return "", err
+		return []Series{}, err
 	}
 
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	page := string(body)
-	re := regexp.MustCompile(`href="/mangas/(\d*)/(.*?)">\s*([^<]+)\s*<`)
-	matches := re.FindAllStringSubmatch(page, -1)
+	var re *regexp.Regexp = regexp.MustCompile(`href="/mangas/(\d*)/(.*?)">\s*([^<]+)\s*<`)
+	var matches [][]string = re.FindAllStringSubmatch(page, -1)
 	for i := range matches {
 		fmt.Printf("%s %s %s\n", matches[i][1], matches[i][2], matches[i][3])
 	}
 
-	return "", nil
+	return []Series{}, nil
+}
+
+func getSeriesPage() (string, error) {
+	resp, err := http.Get("https://onepiecechapters.com/projects")
+	if err == nil {
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err == nil {
+			return string(body), nil
+		}
+	}
+	return "", err
 }
