4d94fc02788ef01446e439116eac4a3e282b84ae
Kevin Ramdath <krpent@gmail.com>
2022-07-21 11:18:24 -0400

    use generic get html page

--- pkg/series.go
+++ pkg/series.go
@@ -2,8 +2,6 @@ package pkg
 
 import (
 	"errors"
-	"io"
-	"net/http"
 	"regexp"
 )
 
@@ -16,13 +14,14 @@ type Series struct {
 
 // GetSeries returns a slice of all the series found on the TCBScans website.
 func GetSeries() ([]Series, error) {
-	page, err := getSeriesPage()
+	var url string = "https://onepiecechapters.com/projects"
+	html, err := getPage(url)
 	if err != nil {
 		return nil, err
 	}
 
 	var re *regexp.Regexp = regexp.MustCompile(`href="/mangas/(\d*)/(.*?)">\s*([^<]+)\s*<`)
-	var matches [][]string = re.FindAllStringSubmatch(page, -1)
+	var matches [][]string = re.FindAllStringSubmatch(html, -1)
 	if len(matches) == 0 {
 		return nil, errors.New("unable to parse series from page")
 	}
@@ -37,18 +36,4 @@ func GetSeries() ([]Series, error) {
 	}
 
 	return series, nil
-}
-
-// getSeriesPage fetches the html page from TCBScans containing the list of series.
-func getSeriesPage() (string, error) {
-	resp, err := http.Get("https://onepiecechapters.com/projects")
-	if err == nil {
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		if err == nil {
-			return string(body), nil
-		}
-	}
-	return "", err
 }
