7b64292d6dc7ed571e51067e0c8a1f1e0771809c
Kevin Ramdath <krpent@gmail.com>
2022-07-22 07:50:31 -0400

    save page from url

--- pkg/utils.go
+++ pkg/utils.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 // getPage returns the html page for the given url.
@@ -17,4 +18,26 @@ func getPage(url string) (string, error) {
 		}
 	}
 	return "", err
+}
+
+// savePage saves the response body to the given path.
+func savePage(url string, filename string) error {
+	resp, err := http.Get(url)
+	if err == nil {
+		defer resp.Body.Close()
+
+		file, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		_, err = io.Copy(file, resp.Body)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+	return err
 }
